5cd24c6d9d5215f29f93e45f88ea72c37072a8c5
Gomez Tomas Gonzalo <talk.gtg@gmail.com>
2023-04-30 13:37:31 -0300

    Sumo seeder de metodo de contacto

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             ProfessionSeeder::class,
             SpecialitySeeder::class,
+            MethodContactSeeder::class
         ]);
     }
 }
