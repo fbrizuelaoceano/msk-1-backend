bfa7379cc8969dba19da1cbafbb4718f8b01ed1a
Gomez Tomas Gonzalo <talk.gtg@gmail.com>
2023-05-09 19:26:25 -0300

    dump de post

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -27,8 +27,8 @@ class AuthController extends Controller
         ]);
         $data = json_decode($request->getContent(), true);
         $contact = collect($request->contact)->toArray();
-        dump($_POST)
-        Log::info($data->FirstName);
+        Log::info(print_r($_POST, true));
+        Log::info(print_r($data->FirstName, true));
 
         $user = User::updateOrCreate(['email' => $request->email], [
             'name' => $contact['First_Name'],
