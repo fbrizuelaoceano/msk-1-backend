f1d5ef9ed9deb075df9019f0b9f5906d1659ee08
flidas-oceano <talk.tomas@hotmail.com>
2023-05-10 11:20:02 -0300

    Seeder trunca la tabla

--- database/seeders/ProfessionSeeder.php
+++ database/seeders/ProfessionSeeder.php
@@ -28,7 +28,9 @@ class ProfessionSeeder extends Seeder
      */
     public function run()
     {
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0;'); // Desactivamos la revisión de claves foráneas
         DB::table('professions')->truncate();
+        DB::statement('SET FOREIGN_KEY_CHECKS = 1;'); // Reactivamos la revisión de claves foráneas
         foreach ($this->data as $d) {
             Profession::create($d);
         }
