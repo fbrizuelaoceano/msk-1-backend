22a9eaa7567e3f6f891683a6c370429f5205c785
Gomez Tomas Gonzalo <talk.gtg@gmail.com>
2023-05-12 11:31:06 -0300

    contact_id lo paso com un id comun

--- database/migrations/2023_04_18_163220_create_contracts_table.php
+++ database/migrations/2023_04_18_163220_create_contracts_table.php
@@ -12,9 +12,8 @@ return new class extends Migration {
     {
         Schema::create('contracts', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('contact_id')
-                ->references('id')
-                ->on('contacts')->onDelete('cascade')->onUpdate('cascade')->nullable()->default(null);
+            $table->unsignedBigInteger('contact_id')->nullable()->default(null);
+
             $table->string('installments')->nullable()->default(null);
             $table->string('entity_id_crm')->nullable()->default(null);
             $table->string('Fecha_de_Vto')->nullable()->default(null);
