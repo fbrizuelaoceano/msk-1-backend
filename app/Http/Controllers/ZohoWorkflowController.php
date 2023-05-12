d717347387d461a97c475893cc8d5a732e734103
Gomez Tomas Gonzalo <talk.gtg@gmail.com>
2023-05-12 11:04:54 -0300

    Viendo que viaja ahora en contacto

--- app/Http/Controllers/ZohoWorkflowController.php
+++ app/Http/Controllers/ZohoWorkflowController.php
@@ -36,8 +36,8 @@ class ZohoWorkflowController extends Controller
         $contactObj = json_decode($_POST['contact']);
         $saleObj = json_decode($_POST['sale']);
 
-        Log::info(print_r($contactObj));
-        Log::info(print_r($saleObj));
+        Log::info(print_r($contactObj, true));
+        Log::info(print_r($saleObj, true));
 
         dd($contactObj->Usuario);
 
