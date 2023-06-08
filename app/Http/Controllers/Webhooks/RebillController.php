2f1286180444166e5807869b5bde83dcb11ae006
Gomez Tomas Gonzalo <talk.gtg@gmail.com>
2023-06-08 17:41:18 -0300

    json de webhok

--- app/Http/Controllers/Webhooks/RebillController.php
+++ app/Http/Controllers/Webhooks/RebillController.php
@@ -11,24 +11,24 @@ class RebillController extends Controller
     public function newPayment(Request $request)
     {
         $jsonPayload = file_get_contents('php://input');
-        $data = json_decode($jsonPayload);
+        $data = json_decode($jsonPayload, true);
 
-        Log::info("newPayment: " . print_r($data));
+        Log::info("newPayment: " . print_r($data, true));
     }
 
     public function newSubscription(Request $request)
     {
         $jsonPayload = file_get_contents('php://input');
-        $data = json_decode($jsonPayload);
+        $data = json_decode($jsonPayload, true);
 
-        Log::info("newSubscription: " . print_r($data));
+        Log::info("newSubscription: " . print_r($data, true));
     }
 
     public function changeStatusPayment(Request $request)
     {
         $jsonPayload = file_get_contents('php://input');
-        $data = json_decode($jsonPayload);
+        $data = json_decode($jsonPayload, true);
 
-        Log::info("changeStatusPayment: " . print_r($data));
+        Log::info("changeStatusPayment: " . print_r($data, true));
     }
 }
