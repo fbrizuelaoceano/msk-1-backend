601e548ef4299dbee1d6aad5ceadb6a1d1ef3a23
fbrizuelaoceano <facundobrizuela@oceano.com.ar>
2023-06-09 16:15:23 -0300

    get paymentlinbyemail

--- app/Http/Controllers/Webhooks/RebillController.php
+++ app/Http/Controllers/Webhooks/RebillController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Webhooks;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
 class RebillController extends Controller
 {
@@ -16,25 +17,40 @@ class RebillController extends Controller
         Log::info("newPayment: " . print_r($data, true));
 
         $id = $data['payment']['id'];
+        $email = $data['payment']['customer']['email'];
+        $status = $data['payment']['status'];
+
+        $paymentLink = DB::connection('omApiPayments')->select('SELECT *
+        FROM laravel_api_payments.rebill_customers AS rebill_c
+        INNER JOIN laravel_api_payments.payment_links AS pay_l ON rebill_c.id = pay_l.rebill_customer_id
+        WHERE rebill_c.email = '.$email.'
+        ORDER BY rebill_c.created_at DESC
+        LIMIT 1;');
+
+        Log::info("paymentLink get by email: " . print_r($paymentLink , true));
+
+
+        // $token = "API_KEY_955a1b47-1b02-4f09-af6b-5be66da4d8d4";        
+
+        // //->apipaymentlink.->update($newStatus)->where("",$)
+
+
+        // $response = Http::withHeaders([
+        //     'Accept' => 'application/json',
+        //     'Authorization' => 'Bearer '.$token
+        // ])->get('https://api.rebill.to/v2/payments/'.$id)->json();
         
-        $token = "API_KEY_955a1b47-1b02-4f09-af6b-5be66da4d8d4";
+        // Log::info("response getPaymentById: " . print_r($response, true));
 
-        $response = Http::withHeaders([
-            'Accept' => 'application/json',
-            'Authorization' => 'Bearer '.$token
-        ])->get('https://api.rebill.to/v2/payments/'.$id)->json();
-        
-        Log::info("response getPaymentById: " . print_r($response, true));
-
-        if ($response->failed()) {
-            Log::info("Error, Response, getPayMentByID, changeStatusPayment: " . print_r($response, true));
-            // echo "HTTP Error: " . $response->status();
-        } else {
-            // echo $response->body();
-            // Log::info("bodyPayment, Response, getPayMentByID, changeStatusPayment: " . print_r($response, true));
+        // if ($response->failed()) {
+        //     Log::info("Error, Response, getPayMentByID, changeStatusPayment: " . print_r($response, true));
+        //     // echo "HTTP Error: " . $response->status();
+        // } else {
+        //     // echo $response->body();
+        //     // Log::info("bodyPayment, Response, getPayMentByID, changeStatusPayment: " . print_r($response, true));
 
 
-        }
+        // }
 
     }
 
@@ -53,21 +69,7 @@ class RebillController extends Controller
 
         Log::info("changeStatusPayment: " . print_r($data, true));
 
-        $exampleRequestWebhook = '{
-            "payment": {
-             "id": "024160d0-7ba4-48c0-828e-b2c6fab86e53",
-             "createdAt": "2022-09-06T14:38:00.082Z",
-             "newStatus": "REFUNDED",
-             "orderId": "637285ca-85f5-476e-bade-40b6ec61da2c",
-             "previousStatus": "SUCCEEDED"
-            },
-             "webhook":{
-                "id":"bf8d33b1-2f0b-4efc-bc02-bf9d3a04a850",
-                "event":"payment-change-status",
-                "url":"https://your.domain/payment-change-status",
-                "logId":"eeb168f-ed10-4619-9eb2-a48b8a6744e2"
-             }
-        }';
+        
 
         $id = $data['payment']['id'];
 
