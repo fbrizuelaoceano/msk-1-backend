9b2064f1e31729acd3b32442ab759354f9583ee2
Gomez Tomas Gonzalo <talk.gtg@gmail.com>
2023-07-26 17:19:13 -0300

    Telescope

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Foundation\Auth\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Tomas Gomez',
+            'email' => 'tomasgomez@msk.com.ar',
+            'password' => Hash::make('password')
+        ]);
+    }
+}
