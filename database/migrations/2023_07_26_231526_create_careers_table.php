21889b4055ac19ac8a3d6619f61a8ed3526fd9ea
fbrizuelaoceano <facundobrizuela@oceano.com.ar>
2023-07-27 00:55:40 -0300

    Nueva tabla carrera

---
+++ database/migrations/2023_07_26_231526_create_careers_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('careers', function (Blueprint $table) {
+            $table->id();
+            $table->integer('year');
+            $table->foreignId('contact_id')->references('id')->on('contacts');
+            $table->foreignId('profession_id')->references('id')->on('professions');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('careers');
+    }
+};
