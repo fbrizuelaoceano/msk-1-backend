9ee5291b30965492e444373d64a6ed205c0e15b4
Gomez Tomas Gonzalo <talk.gtg@gmail.com>
2023-09-29 14:48:41 -0300

    Preferencia de Contactacion ahora no es obligatorio

--- app/Http/Requests/ContactUsRequest.php
+++ app/Http/Requests/ContactUsRequest.php
@@ -74,8 +74,6 @@ class ContactUsRequest extends FormRequest
             'First_Name' => 'required|string',
             'Pais' => 'required|string',
             'Phone' => 'required|string',
-
-            'Preferencia_de_contactaci_n' => 'string',
             'Profesion' => 'required',
             'Especialidad' => 'required_unless:Profesion,Estudiante',
             'Otra_profesion' => 'required_if:Profesion,Otra profesión',
